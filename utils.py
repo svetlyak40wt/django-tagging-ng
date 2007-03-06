ce152f15065bbea28f6e57be0437094de3b763f6
jonathan.buchanan <jonathan.buchanan@art-laptop>
2007-03-06 22:20:35 +0000

    [django-tagging @ 22] Tag name lists may now be delimited with commas as well as spaces; added some doctests

--- utils.py
+++ utils.py
@@ -1,4 +1,10 @@
-import urllib
+import re
+
+# Python 2.3 compatibility
+if not hasattr(__builtins__, 'set'):
+    from sets import Set as set
+
+split_tag_re = re.compile('[\s,]+')
 
 class DummyTag:
     def __init__(self, name):
@@ -9,13 +15,31 @@ class DummyTag:
 
 def create_dummy_tags(tag_list):
     """
-    Given a space-delimited string of tag names, creates a list of
-    dummy tag objects which contain name attributes.
+    Given a string of tag names, creates a list of dummy tag objects
+    with ``name`` attributes.
 
     This can be used to reuse an item's list of tag names for display
     purposes instead of loading tags from the database.
     """
-    tag_names = []
-    if tag_list != None:
-        tag_names = set(tag_list.split())
+    tag_names = set(split_tag_list(tag_list))
     return [DummyTag(tag_name) for tag_name in tag_names]
+
+def split_tag_list(tag_list):
+    """
+    Split the given list of tags on whitespace and commas and return
+    a list of tags.
+
+    >>> split_tag_list(None)
+    []
+    >>> split_tag_list('')
+    []
+    >>> split_tag_list('foo')
+    ['foo']
+    >>> split_tag_list('foo bar')
+    ['foo', 'bar']
+    >>> split_tag_list('foo,bar')
+    ['foo', 'bar']
+    >>> split_tag_list(',  , foo   ,   bar ,  ,baz, , ,')
+    ['foo', 'bar', 'baz']
+    """
+    return [t for t in split_tag_re.split(tag_list or '') if t]
--- validators.py
+++ validators.py
@@ -1,12 +1,44 @@
 import re
 from django.core.validators import ValidationError
+from tagging.utils import split_tag_list
 
-tag_list_re = re.compile(r'^[- \w]+$')
+tag_list_re = re.compile(r'^[-\w]+(?:(?:,\s|[,\s])[-\w]+)*$')
 
 def isTagList(field_data, all_data):
+    """
+    Validates that ``field_data`` is a valid list of tag names,
+    separated by a single comma, a single space or a comma followed
+    by a space.
+
+    >>> isTagList('foo', {})
+    >>> isTagList('foo bar baz', {})
+    >>> isTagList('foo,bar,baz', {})
+    >>> isTagList('foo, bar, baz', {})
+    >>> isTagList('foo qwertyuiopasdfghjklzxcvbnmqwertyuiopasdfghjklzxcvb bar', {})
+    >>> isTagList('', {})
+    Traceback (most recent call last):
+        ...
+    ValidationError: ['Tag names must contain only letters, numbers, underscores or hyphens, with a comma, space or comma followed by space used to separate each tag name.']
+    >>> isTagList(' foo', {})
+    Traceback (most recent call last):
+        ...
+    ValidationError: ['Tag names must contain only letters, numbers, underscores or hyphens, with a comma, space or comma followed by space used to separate each tag name.']
+    >>> isTagList('foo ', {})
+    Traceback (most recent call last):
+        ...
+    ValidationError: ['Tag names must contain only letters, numbers, underscores or hyphens, with a comma, space or comma followed by space used to separate each tag name.']
+    >>> isTagList('foo  bar', {})
+    Traceback (most recent call last):
+        ...
+    ValidationError: ['Tag names must contain only letters, numbers, underscores or hyphens, with a comma, space or comma followed by space used to separate each tag name.']
+    >>> isTagList('foo qwertyuiopasdfghjklzxcvbnmqwertyuiopasdfghjklzxcvbn bar', {})
+    Traceback (most recent call last):
+        ...
+    ValidationError: ['Tag names must be no longer than 50 characters.']
+    """
     if not tag_list_re.search(field_data):
-        raise ValidationError, 'This value must contain only letters, numbers, underscores, hyphens or spaces.'
-    tag_names = field_data.split()
+        raise ValidationError('Tag names must contain only letters, numbers, underscores or hyphens, with a comma, space or comma followed by space used to separate each tag name.')
+    tag_names = split_tag_list(field_data)
     for tag_name in tag_names:
         if len(tag_name) > 50:
-            raise ValidationError, 'Each tag name must be no longer than 50 characters.'
+            raise ValidationError('Tag names must be no longer than 50 characters.')
