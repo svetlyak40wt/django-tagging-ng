845c6b7de10c0e5cf82f253612450323f6885849
jonathan.buchanan <jonathan.buchanan@83e7428b-ec2a-0410-86f2-bf466d0e5e72>
2007-05-13 23:24:11 +0000

    Added TaggedItemManager.get_related
    git-svn-id: http://django-tagging.googlecode.com/svn/trunk@48 83e7428b-ec2a-0410-86f2-bf466d0e5e72

--- tests/models.py
+++ tests/models.py
@@ -9,3 +9,21 @@ class Parrot(models.Model):
 
     class Meta:
         ordering = ['state']
+
+class Link(models.Model):
+    name = models.CharField(maxlength=50)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        ordering = ['name']
+
+class Article(models.Model):
+    name = models.CharField(maxlength=50)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        ordering = ['name']
