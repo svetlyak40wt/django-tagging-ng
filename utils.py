388dfe5cb8c8baaea90ac88a289ece9ff77b4c33
jonathan.buchanan <jonathan.buchanan@art-laptop>
2007-05-16 23:00:26 +0000

    [django-tagging @ 53] Removed reduntant dummy tag code from utils

--- utils.py
+++ utils.py
@@ -7,24 +7,6 @@ if not hasattr(__builtins__, 'set'):
 
 find_tag_re = re.compile('[-\w]+', re.U)
 
-class DummyTag:
-    def __init__(self, name):
-        self.name = name
-
-    def __str__(self):
-        return self.name
-
-def create_dummy_tags(tag_list):
-    """
-    Given a string of tag names, creates a list of dummy tag objects
-    with ``name`` attributes.
-
-    This can be used to reuse an item's list of tag names for display
-    purposes instead of loading tags from the database.
-    """
-    tag_names = set(split_tag_list(tag_list))
-    return [DummyTag(tag_name) for tag_name in tag_names]
-
 def get_tag_name_list(tag_names):
     """
     Find tag names in the given string and return them as a list.
