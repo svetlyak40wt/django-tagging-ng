6b1a27898b84bbd1b0e89b061802668a5c7562cb
jonathan.buchanan <jonathan.buchanan@art-laptop>
2007-08-06 14:22:53 +0000

    [django-tagging @ 87] A backwards-incompatible change was made to run_tests in Django 5769

--- tagging/tests/runtests.py
+++ tagging/tests/runtests.py
@@ -3,9 +3,7 @@ os.environ['DJANGO_SETTINGS_MODULE'] = 'tagging.tests.settings'
 
 from django.test.simple import run_tests
 from django.conf import settings
-from django.db.models.loading import load_app
 
-test_models = [load_app(app) for app in settings.INSTALLED_APPS]
-failures = run_tests(test_models, verbosity=9)
+failures = run_tests(None, verbosity=9)
 if failures:
     sys.exit(failures)
