02b2f0531f0976066d39c070861ee622dc266bce
jonathan.buchanan <jonathan.buchanan@83e7428b-ec2a-0410-86f2-bf466d0e5e72>
2007-08-20 21:48:11 +0000

    Added related_tags and related_tag_counts arguments to the tagged_object_list generic view
    git-svn-id: http://django-tagging.googlecode.com/svn/trunk@96 83e7428b-ec2a-0410-86f2-bf466d0e5e72

--- tagging/views.py
+++ tagging/views.py
@@ -1,10 +1,11 @@
 from django.http import Http404
 from django.views.generic.list_detail import object_list
 
-from tagging.models import TaggedItem
+from tagging.models import Tag, TaggedItem
 from tagging.utils import get_tag
 
-def tagged_object_list(request, model=None, tag=None, **kwargs):
+def tagged_object_list(request, model=None, tag=None, related_tags=False,
+        related_tag_counts=True, **kwargs):
     """
     A thin wrapper around
     ``django.views.generic.list_detail.object_list`` which creates a
@@ -14,6 +15,12 @@ def tagged_object_list(request, model=None, tag=None, **kwargs):
     In addition to the context variables set up by ``object_list``, a
     ``tag`` context variable will contain the ``Tag`` instance
     for the given tag.
+
+    If ``related_tags`` is ``True``, a ``related_tags`` context variable
+    will contain tags related to the given tag for the given model.
+    Additionally, if ``related_tag_counts`` is ``True``, each related tag
+    will have a ``count`` attribute indicating the number of items which
+    have it in addition to the given tag.
     """
     if model is None:
         try:
@@ -34,4 +41,8 @@ def tagged_object_list(request, model=None, tag=None, **kwargs):
     if not kwargs.has_key('extra_context'):
         kwargs['extra_context'] = {}
     kwargs['extra_context']['tag'] = tag_instance
+    if related_tags:
+        kwargs['extra_context']['related_tags'] = \
+            Tag.objects.related_for_model(tag_instance, model,
+                                          counts=related_tag_counts)
     return object_list(request, queryset, **kwargs)
