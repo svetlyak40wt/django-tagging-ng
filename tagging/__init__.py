676778d07c10fa096eee66ace5481fc3abe44b12
jonathan.buchanan <jonathan.buchanan@art-laptop>
2008-01-14 20:14:07 +0000

    [django-tagging @ 126] A descriptor which provides access to tags is now added to models which are registered

--- tagging/__init__.py
+++ tagging/__init__.py
@@ -1,6 +1,6 @@
 from django.utils.translation import ugettext as _
 
-from tagging.managers import ModelTaggedItemManager
+from tagging.managers import ModelTagManager, ModelTaggedItemManager
 
 VERSION = (0, 3, 'pre')
 
@@ -12,7 +12,8 @@ class AlreadyRegistered(Exception):
 
 registry = []
 
-def register(model, tagged_item_manager_attr='tagged'):
+def register(model, tag_descriptor_attr='tags',
+             tagged_item_manager_attr='tagged'):
     """
     Sets the given model class up for working with tags.
     """
@@ -21,6 +22,29 @@ def register(model, tagged_item_manager_attr='tagged'):
             _('The model %s has already been registered with tagging.') % model.__name__)
     registry.append(model)
 
+    # Add tag descriptor
+    setattr(model, tag_descriptor_attr, TagDescriptor())
+
     # Add custom manager
     ModelTaggedItemManager().contribute_to_class(model,
                                                  tagged_item_manager_attr)
+
+class TagDescriptor(object):
+    """
+    A descriptor which provides access to a ``ModelTagManager`` for
+    model classes and simple retrieval, updating and deletion of tags
+    for model instances.
+    """
+    def __get__(self, instance, owner):
+        if not instance:
+            tag_manager = ModelTagManager()
+            tag_manager.model = owner
+            return tag_manager
+        else:
+            return Tag.objects.get_for_object(instance)
+
+    def __set__(self, instance, value):
+        Tag.objects.update_tags(instance, value)
+
+    def __del__(self, instance):
+        Tag.objects.update_tags(instance, None)
--- tagging/managers.py
+++ tagging/managers.py
@@ -2,16 +2,32 @@
 Custom managers for Django models registered with the tagging
 application.
 """
-from django.db import models
+from django.contrib.contenttypes.models import ContentType
+from django.db import connection, models
 
-from tagging.models import TaggedItem
+from tagging.models import Tag, TaggedItem
+
+class ModelTagManager(models.Manager):
+    """
+    A manager for retrieving tags for a particular model.
+    """
+    def get_query_set(self):
+        ctype = ContentType.objects.get_for_model(self.model)
+        return Tag.objects.filter(
+            items__content_type__pk=ctype.pk).distinct()
+
+    def cloud(self, *args, **kwargs):
+        return Tag.objects.cloud_for_model(self.model, *args, **kwargs)
+
+    def related(self, tags, *args, **kwargs):
+        return Tag.objects.related_for_model(tags, self.model, *args, **kwargs)
 
 class ModelTaggedItemManager(models.Manager):
     """
     A manager for retrieving model instances based on their tags.
     """
-    def related_to(self, obj):
-        return TaggedItem.objects.get_related(obj, self.model)
+    def related_to(self, obj, *args, **kwargs):
+        return TaggedItem.objects.get_related(obj, self.model, *args, **kwargs)
 
     def with_all(self, tags):
         return TaggedItem.objects.get_by_model(self.model, tags)
