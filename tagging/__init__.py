67b165bba2f813ba57a7e73f40afa48556d1d199
jonathan.buchanan <jonathan.buchanan@83e7428b-ec2a-0410-86f2-bf466d0e5e72>
2008-01-14 20:12:54 +0000

    Moved tagging managers to models.py; added ability to register models with the tagging module
    git-svn-id: http://django-tagging.googlecode.com/svn/trunk@125 83e7428b-ec2a-0410-86f2-bf466d0e5e72

--- tagging/__init__.py
+++ tagging/__init__.py
@@ -1,1 +1,26 @@
-VERSION = (0, 2, None)
+from django.utils.translation import ugettext as _
+
+from tagging.managers import ModelTaggedItemManager
+
+VERSION = (0, 3, 'pre')
+
+class AlreadyRegistered(Exception):
+    """
+    An attempt was made to register a model more than once.
+    """
+    pass
+
+registry = []
+
+def register(model, tagged_item_manager_attr='tagged'):
+    """
+    Sets the given model class up for working with tags.
+    """
+    if model in registry:
+        raise AlreadyRegistered(
+            _('The model %s has already been registered with tagging.') % model.__name__)
+    registry.append(model)
+
+    # Add custom manager
+    ModelTaggedItemManager().contribute_to_class(model,
+                                                 tagged_item_manager_attr)
