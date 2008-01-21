2a6450452af0357b254043d63ad2879b0c3877de
jonathan.buchanan <jonathan.buchanan@art-laptop>
2008-01-21 17:55:31 +0000

    [django-tagging @ 130] TaggedItemManager's methods now accept a QuerySet or a Model class

--- tagging/__init__.py
+++ tagging/__init__.py
@@ -1,6 +1,6 @@
 from django.utils.translation import ugettext as _
 
-from tagging.managers import ModelTagManager, ModelTaggedItemManager
+from tagging.managers import ModelTaggedItemManager, TagDescriptor
 
 VERSION = (0, 3, 'pre')
 
@@ -19,7 +19,7 @@ def register(model, tag_descriptor_attr='tags',
     """
     if model in registry:
         raise AlreadyRegistered(
-            _('The model %s has already been registered with tagging.') % model.__name__)
+            _('The model %s has already been registered.') % model.__name__)
     registry.append(model)
 
     # Add tag descriptor
@@ -28,23 +28,3 @@ def register(model, tag_descriptor_attr='tags',
     # Add custom manager
     ModelTaggedItemManager().contribute_to_class(model,
                                                  tagged_item_manager_attr)
-
-class TagDescriptor(object):
-    """
-    A descriptor which provides access to a ``ModelTagManager`` for
-    model classes and simple retrieval, updating and deletion of tags
-    for model instances.
-    """
-    def __get__(self, instance, owner):
-        if not instance:
-            tag_manager = ModelTagManager()
-            tag_manager.model = owner
-            return tag_manager
-        else:
-            return Tag.objects.get_for_object(instance)
-
-    def __set__(self, instance, value):
-        Tag.objects.update_tags(instance, value)
-
-    def __del__(self, instance):
-        Tag.objects.update_tags(instance, None)
--- tagging/utils.py
+++ tagging/utils.py
@@ -124,6 +124,19 @@ def edit_string_for_tags(tags):
         glue = u' '
     return glue.join(names)
 
+def get_queryset_and_model(queryset_or_model):
+    """
+    Given a ``QuerySet`` or a ``Model``, returns a two-tuple of
+    (queryset, model).
+
+    If a ``Model`` is given, the ``QuerySet`` returned will be created
+    using its default manager.
+    """
+    try:
+        return queryset_or_model, queryset_or_model.model
+    except AttributeError:
+        return queryset_or_model._default_manager.all(), queryset_or_model
+
 def get_tag_list(tags):
     """
     Utility function for accepting tag input in a flexible manner.
