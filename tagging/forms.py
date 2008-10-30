6292d57da31b9302cb415b747ad18155cf22f3bf
jonathan.buchanan <jonathan.buchanan@83e7428b-ec2a-0410-86f2-bf466d0e5e72>
2008-10-30 02:17:55 +0000

    Got tests back in working order; documentation updates for Django 1.0
    git-svn-id: http://django-tagging.googlecode.com/svn/trunk@151 83e7428b-ec2a-0410-86f2-bf466d0e5e72

--- tagging/forms.py
+++ tagging/forms.py
@@ -1,22 +1,27 @@
 """
-Tagging components for Django's ``newforms`` form library.
+Tagging components for Django's form library.
 """
 from django import forms
 from django.utils.translation import ugettext as _
 
 from tagging import settings
 from tagging.models import Tag
-from tagging.validators import is_tag, is_tag_list
 from tagging.utils import parse_tag_input
 
 class AdminTagForm(forms.ModelForm):
     class Meta:
         model = Tag
-    
+
     def clean_name(self):
-        value = self.cleaned_data["name"]
-        return is_tag(value)
-        
+        value = self.cleaned_data['name']
+        tag_names = parse_tag_input(value)
+        if len(tag_names) > 1:
+            raise ValidationError(_('Multiple tags were given.'))
+        elif len(tag_names[0]) > settings.MAX_TAG_LENGTH:
+            raise forms.ValidationError(
+                _('A tag may be no more than %s characters long.') %
+                    settings.MAX_TAG_LENGTH)
+        return value
 
 class TagField(forms.CharField):
     """
@@ -27,4 +32,9 @@ class TagField(forms.CharField):
         value = super(TagField, self).clean(value)
         if value == u'':
             return value
-        return is_tag_list(value)
+        for tag_name in parse_tag_input(value):
+            if len(tag_name) > settings.MAX_TAG_LENGTH:
+                raise forms.ValidationError(
+                    _('Each tag may be no more than %s characters long.') %
+                        settings.MAX_TAG_LENGTH)
+        return value
