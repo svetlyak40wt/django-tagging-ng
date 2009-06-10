e2ec2bf639a4f0c69e85a68c05e3ab52cc5495a5
Alexander Artemenko <svetlyak.40wt@gmail.com>
2009-06-10 16:44:55 -0400

    Avanced admin list for Tag model.

--- tagging/admin.py
+++ tagging/admin.py
@@ -1,6 +1,26 @@
 from django.contrib import admin
 from tagging.models import Tag, TaggedItem, Synonym
+from django.utils.translation import ugettext as _
+from tagging import settings
 
 admin.site.register(TaggedItem)
-admin.site.register(Tag)
+
+if settings.MULTILINGUAL_TAGS:
+    def _name(tag):
+        return tag.name_any
+    _name.short_description = _('name')
+
+    def _synonyms(tag):
+        return ', '.join(s.name for s in tag.synonyms.all())
+    _synonyms.short_description = _('synonyms')
+
+    _fields = (_name, _synonyms)
+else:
+    _fields = ('name',)
+
+
+admin.site.register(Tag,
+    list_display = _fields,
+)
 admin.site.register(Synonym)
+
